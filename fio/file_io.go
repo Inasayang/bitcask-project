fc9621d4c3643dc03f2545c7c7a3be5a86dd8c0b
Inasa <xiayangzzw@gmail.com>
2023-05-04 16:06:52 +0800

    feat: Add IO interface and implement(Disk design)

---
+++ fio/file_io.go
@@ -0,0 +1,36 @@
+package fio
+
+import "os"
+
+const (
+	DataFilePerm = 0644
+)
+
+// FileIO is a file IO manager
+type FileIO struct {
+	fd *os.File
+}
+
+func (f *FileIO) Read(bytes []byte, i int64) (int, error) {
+	return f.fd.ReadAt(bytes, i)
+}
+
+func (f *FileIO) Write(bytes []byte) (int, error) {
+	return f.fd.Write(bytes)
+}
+
+func (f *FileIO) Sync() error {
+	return f.fd.Sync()
+}
+
+func (f *FileIO) Close() error {
+	return f.fd.Close()
+}
+
+func NewFileIOManager(fn string) (*FileIO, error) {
+	fd, err := os.OpenFile(fn, os.O_CREATE|os.O_RDWR|os.O_APPEND, DataFilePerm)
+	if err != nil {
+		return nil, err
+	}
+	return &FileIO{fd}, nil
+}
