ce464de8d2ee9be3c3e22e30bc5774bda0719e72
Inasa <xiayangzzw@gmail.com>
2023-05-06 21:52:20 +0800

    feat: Add LogRecordType and LogRecord

--- data/log_record.go
+++ data/log_record.go
@@ -1,5 +1,25 @@
 package data
 
+type LogRecordType = byte
+
+const (
+	LogRecordNormal LogRecordType = iota
+	LogRecordDeleted
+)
+
+func EncodeLogRecord(lr *LogRecord) ([]byte, int64) {
+	//TODO
+	return nil, 0
+}
+
+// LogRecord is a log record 写入到数据文件的记录
+// 数据文件中的数据是追加写入，WAL
+type LogRecord struct {
+	Key   []byte
+	Value []byte
+	Type  LogRecordType
+}
+
 // LogRecordPos is the position of a log record in the log file
 // 数据内存索引
 type LogRecordPos struct {
